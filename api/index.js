1800391cad3d3294a277af15c9e7c6dc03808315
Cherry Ng <cherry92@gmail.com>
2017-02-15 23:43:16 +0800

    Setup first http endpoint

---
+++ api/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.exports = {
+  helloWorld(event, context, callback) {
+    const response = {
+      statusCode: 200,
+      body: 'Hello world!'
+    }
+    callback(null, response)
+  }
+}
