f00143e50be0886f278ce79aca5b83a63aa6a41f
Cherry Ng <cherry92@gmail.com>
2017-02-17 23:23:48 +0800

    Setup wifi connection

--- mcu/init.lua
+++ mcu/init.lua
@@ -1,1 +1,3 @@
-print("Hello World")
+local init_wifi = require "init_wifi"
+
+init_wifi:init()
---
+++ mcu/init_wifi.lua
@@ -0,0 +1,15 @@
+local config = require "wifi_config"
+
+local init_wifi = {}
+
+init_wifi.init = function ()
+  if config.enable_ap then
+    wifi.setmode(wifi.STATIONAP)
+    wifi.ap.config(config.ap)
+  else
+    wifi.setmode(wifi.STATION)
+  end
+  wifi.sta.config(config.sta)
+end
+
+return init_wifi
