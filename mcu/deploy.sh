6b3dcf5172a1c65786e5f806e0cc4e0f9e54b756
Cherry Ng <cherry92@gmail.com>
2017-02-17 22:05:03 +0800

    Create deploy script for mcu

---
+++ mcu/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Deploy script to upload lua program to NodeMcu.
+# TODO: Remove unused files
+# TODO: Use single connection to upload all files
+
+# Upload .lua files to Mcu
+LUA_FILES=`ls -A | grep "\.lua$"`
+for lua_file in $LUA_FILES; do
+  nodemcu-uploader upload $lua_file
+done
