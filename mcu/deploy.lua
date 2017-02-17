1bec43270dcb4e7de1705fd455e752e541ef3457
Cherry Ng <cherry92@gmail.com>
2017-02-17 22:54:01 +0800

    Fix deployment flow

---
+++ mcu/deploy.lua
@@ -0,0 +1,2 @@
+print("Deployment finished, restarting NodeMcu...")
+node.restart()
--- mcu/deploy.sh
+++ mcu/deploy.sh
@@ -8,3 +8,6 @@ LUA_FILES=`ls -A | grep "\.lua$"`
 for lua_file in $LUA_FILES; do
   nodemcu-uploader upload $lua_file
 done
+
+# Restart NodeMcu after deployment
+nodemcu-uploader file do deploy.lua
