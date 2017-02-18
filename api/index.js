27a721c4f942dcacb55dbad2b8e5ce8baad92ce0
Cherry Ng <cherry92@gmail.com>
2017-02-18 22:20:53 +0800

    Send pong in ping handler

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ module.exports = {
   ping(event, context, callback) {
     const response = {
       statusCode: 200,
-      body: JSON.stringify({ status: 200 })
+      body: "pong"
     }
     callback(null, response)
   }
