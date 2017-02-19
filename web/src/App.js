aa1bf39660bf4a515036c09f9b56973c5f0a9c0b
Cherry Ng <cherry92@gmail.com>
2017-02-19 10:58:10 +0800

    Menu layout

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,18 @@
+import './App.css'
+
 import React from 'react'
+import { Container, Menu, Segment } from 'semantic-ui-react'
 
 function App() {
   return (
-    <h1>Can you see the sky?</h1>
+    <div>
+      <Container fluid as={Menu} color="blue" inverted fixed="top" stackable>
+        <Menu.Item>BlueSky</Menu.Item>
+      </Container>
+      <Container fluid as={Segment} basic className="main">
+        Put chart here.
+      </Container>
+    </div>
   )
 }
 
