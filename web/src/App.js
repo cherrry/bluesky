e3d90f025e44f52d8e836c3e5c6d04c9f49c8f53
Cherry Ng <cherry92@gmail.com>
2017-02-19 11:40:37 +0800

    Add sample chart config

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,53 @@
 import './App.css'
 
 import React from 'react'
+import { Line as LineChart } from 'react-chartjs-2'
 import { Container, Menu, Segment } from 'semantic-ui-react'
+
+const chartProps = {
+  data: {
+    datasets: [
+      {
+        label: "Particle A",
+        data: [
+          { x: 1000, y: 20 },
+          { x: 1100, y: 50 },
+          { x: 1300, y: 40 },
+          { x: 1500, y: 40 },
+          { x: 1800, y: 45 },
+          { x: 2000, y: 10 }
+        ]
+      },
+      {
+        label: "Particle B",
+        data: [
+          { x: 1000, y: 200 },
+          { x: 1100, y: 500 },
+          { x: 1300, y: 400 },
+          { x: 1500, y: 400 },
+          { x: 1800, y: 450 },
+          { x: 2000, y: 100 }
+        ]
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: { min: 0, max: 500 },
+        gridLines: {}
+      }],
+      xAxes: [{
+        type: 'time',
+        time: {
+          unit: 'second',
+          min: 0,
+          max: 2000
+        }
+      }]
+    }
+  }
+}
 
 function App() {
   return (
@@ -10,7 +56,7 @@ function App() {
         <Menu.Item>BlueSky</Menu.Item>
       </Container>
       <Container fluid as={Segment} basic className="main">
-        Put chart here.
+        <LineChart {...chartProps} />
       </Container>
     </div>
   )
