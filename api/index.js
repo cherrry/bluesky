75c42ff581704ef786082b323a3122e2dd723626
Cherry Ng <cherry92@gmail.com>
2017-02-21 00:12:56 +0800

    Query data from dynamo db

--- api/index.js
+++ api/index.js
@@ -7,11 +7,47 @@ const TableName = 'bluesky_pmdata'
 
 module.exports = {
   get(event, context, callback) {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({ status: 200 })
+    const params = event.queryStringParameters
+
+    const hasDeviceId = params.hasOwnProperty('device_id')
+    const hasTimeRange = params.hasOwnProperty('from') && params.hasOwnProperty('to')
+
+    if (!(hasDeviceId && hasTimeRange)) {
+      const response = {
+        statusCode: 400,
+        body: JSON.stringify({
+          status: 400,
+          message: 'Bad Request'
+        })
+      }
+      callback(null, response)
+      return
     }
-    callback(null, response)
+
+    dynamo.query({
+      TableName,
+      KeyConditionExpression: 'device_id = :device_id AND #timestamp BETWEEN :from AND :to',
+      ExpressionAttributeNames: {
+        '#timestamp': 'timestamp'
+      },
+      ExpressionAttributeValues: {
+        ':device_id': params.device_id,
+        ':from': parseInt(params.from),
+        ':to': parseInt(params.to)
+      }
+    }, function (err, data) {
+      console.log(data.Items)
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(data.Items.map(function (item) {
+          return {
+            timestamp: item.timestamp,
+            readings: item.readings
+          }
+        }))
+      }
+      callback(null, response)
+    })
   },
 
   put(event, context, callback) {
