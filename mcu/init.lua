de0037880140d04b97ae502efa60f4ba7d991e35
Cherry Ng <cherry92@gmail.com>
2017-03-05 00:52:41 +0800

    Remove lua sources

--- mcu/init.lua
+++
@@ -1,9 +0,0 @@
-local init_bluesky = require("init_bluesky")
-local init_sntp = require("init_sntp")
-local init_ssl = require("init_ssl")
-local init_wifi = require("init_wifi")
-
-init_bluesky:init()
-init_sntp:init()
-init_wifi:init()
-init_ssl:init()
--- mcu/pseudo_rng.lua
+++
@@ -1,9 +0,0 @@
-local pseudo_rng = {}
-
-local seed = 123456789
-pseudo_rng.next = function ()
-  seed = (1103515245 * seed + 12345)
-  return seed
-end
-
-return pseudo_rng
